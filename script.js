7fbb2c50be5307d1d9fa37a81ba5fcfa69bb881d
Muhammad Bilal <muhammadbilaal098@gmail.com>
2024-07-04 15:06:18 +0500

    Update script.js

--- script.js
+++ script.js
@@ -56,32 +56,20 @@ function copyToClipboard() {
     alert('Text copied to clipboard!');
 }
 
-function switchTexts() {
-    let textArea1 = document.getElementById('english-textarea');
-    let textArea2 = document.getElementById('morse-textarea');
+function swapElements() {
+    // Target main elements for this function
+    let span1 = document.getElementById('span1');
+    let span2 = document.getElementById('span2');
     let label1 = document.getElementById('label-1');
     let label2 = document.getElementById('label-2');
 
-    // Swap text area values
-    let tempText = textArea1.value;
-    textArea1.value = textArea2.value;
-    textArea2.value = tempText;
 
-    // Swap text area functionalities
-    let tempOninput = textArea1.getAttribute('oninput');
-    textArea1.setAttribute('oninput', textArea2.getAttribute('oninput'));
-    textArea2.setAttribute('oninput', tempOninput);
+    // Clone the spans including all attributes and content
+    let span1Clone = span1.cloneNode(true);
+    let span2Clone = span2.cloneNode(true);
 
-    // Swap labels
-    let tempLabel = label1.innerText;
-    label1.innerText = label2.innerText;
-    label2.innerText = tempLabel;
+    // Replace the original spans with their clones
+    span1.parentNode.replaceChild(span2Clone, span1);
+    span2.parentNode.replaceChild(span1Clone, span2);
 
-    // Ensure both text areas are editable
-    textArea1.removeAttribute('readonly');
-    textArea2.removeAttribute('readonly');
-
- // Toggle the oninput functions
- textArea1.oninput = textArea1.oninput === changeEnglishText ? changeMorseCode : changeEnglishText;
- textArea2.oninput = textArea2.oninput === changeMorseCode ? changeEnglishText : changeMorseCode;
 }
